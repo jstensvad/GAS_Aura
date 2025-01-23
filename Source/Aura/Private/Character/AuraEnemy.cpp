66ad4519af6ebe1ec1807a777aa4e9c2174f8274
Jordan S <110872997+jstensvad@users.noreply.github.com>
2025-01-23 13:03:13 -0800

    2.21

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,7 +2,19 @@
 
 
 #include "Character/AuraEnemy.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
 #include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	AttributeSet=CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+}
 
 void AAuraEnemy::HighlightActor()
 {
@@ -17,3 +29,5 @@ void AAuraEnemy::UnHighlightActor()
 	GetMesh()->SetRenderCustomDepth(false);
 	Weapon->SetRenderCustomDepth(false);
 }
+
+
