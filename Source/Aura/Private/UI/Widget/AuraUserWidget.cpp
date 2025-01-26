d47ba1c0f540ed023f0a0a4c0266f183973e4097
Jordan S <110872997+jstensvad@users.noreply.github.com>
2025-01-26 13:59:13 -0800

    29

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Jordan Stensvad
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
